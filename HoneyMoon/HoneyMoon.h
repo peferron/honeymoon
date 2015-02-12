f9c18cb6ff6757d07243308727331e1bcd0ad285
peferron <pe.ferron@gmail.com>
2015-02-12 05:21:44 -0800

    Cleanup HoneyMoon.h

--- HoneyMoon/HoneyMoon.h
+++ HoneyMoon/HoneyMoon.h
@@ -1,11 +1,3 @@
-//
-//  HoneyMoon.h
-//  HoneyMoon
-//
-//  Created by Mat on 2/12/15.
-//  Copyright (c) 2015 peferron. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 //! Project version number for HoneyMoon.
@@ -15,7 +7,6 @@ FOUNDATION_EXPORT double HoneyMoonVersionNumber;
 FOUNDATION_EXPORT const unsigned char HoneyMoonVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <HoneyMoon/PublicHeader.h>
-
 #import "easing.h"
 #import "EasingDeclarations.h"
 #import "SKEase.h"

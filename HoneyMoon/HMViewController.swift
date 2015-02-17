7275164badae054ef5828d09c3c1c0fcf2987442
peferron <pe.ferron@gmail.com>
2015-02-17 16:41:18 +0800

    Reorganize code in HMViewController

--- HoneyMoon/HMViewController.swift
+++ HoneyMoon/HMViewController.swift
@@ -4,19 +4,6 @@ public class HMViewController: UIViewController, HMSceneDelegate {
     var incoming: SKView?
     var outgoing: SKView?
 
-    let cachedkeyCommands = [
-        UIKeyCommand(input: "w", modifierFlags: nil, action: "didReceiveKeyW"),
-        UIKeyCommand(input: "a", modifierFlags: nil, action: "didReceiveKeyA"),
-        UIKeyCommand(input: "s", modifierFlags: nil, action: "didReceiveKeyS"),
-        UIKeyCommand(input: "d", modifierFlags: nil, action: "didReceiveKeyD"),
-        UIKeyCommand(input: "r", modifierFlags: nil, action: "didReceiveKeyR"),
-        UIKeyCommand(input: "f", modifierFlags: nil, action: "didReceiveKeyF"),
-    ]
-
-    override public var keyCommands: [AnyObject]? {
-        return cachedkeyCommands
-    }
-
     public func presentScene(sceneClass: HMScene.Type) {
         outgoing = incoming
 
@@ -43,27 +30,19 @@ public class HMViewController: UIViewController, HMSceneDelegate {
         presentScene(nextSceneClass)
     }
 
-    // MARK: - UIViewController boilerplate
+    // MARK: - Keys
 
-    override public func shouldAutorotate() -> Bool {
-        return true
-    }
+    let cachedkeyCommands = [
+        UIKeyCommand(input: "w", modifierFlags: nil, action: "didReceiveKeyW"),
+        UIKeyCommand(input: "a", modifierFlags: nil, action: "didReceiveKeyA"),
+        UIKeyCommand(input: "s", modifierFlags: nil, action: "didReceiveKeyS"),
+        UIKeyCommand(input: "d", modifierFlags: nil, action: "didReceiveKeyD"),
+        UIKeyCommand(input: "r", modifierFlags: nil, action: "didReceiveKeyR"),
+        UIKeyCommand(input: "f", modifierFlags: nil, action: "didReceiveKeyF"),
+    ]
 
-    override public func supportedInterfaceOrientations() -> Int {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
-        } else {
-            return Int(UIInterfaceOrientationMask.All.rawValue)
-        }
-    }
-
-    override public func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override public func prefersStatusBarHidden() -> Bool {
-        return true
+    override public var keyCommands: [AnyObject]? {
+        return cachedkeyCommands
     }
 
     override public func canBecomeFirstResponder() -> Bool {
@@ -96,5 +75,28 @@ public class HMViewController: UIViewController, HMSceneDelegate {
 
     func forwardKey(key: String) {
         (incoming?.scene as? HMScene)?.didReceiveKey(key)
+    }
+
+    // MARK: - UIViewController boilerplate
+
+    override public func shouldAutorotate() -> Bool {
+        return true
+    }
+
+    override public func supportedInterfaceOrientations() -> Int {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
+        } else {
+            return Int(UIInterfaceOrientationMask.All.rawValue)
+        }
+    }
+
+    override public func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override public func prefersStatusBarHidden() -> Bool {
+        return true
     }
 }

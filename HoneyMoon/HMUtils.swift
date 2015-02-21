8426e06d91c964bc9fbf5527689b1bcc4601152d
peferron <pe.ferron@gmail.com>
2015-02-22 00:30:39 +0800

    Comment out now-unused Array.every extension

--- HoneyMoon/HMUtils.swift
+++ HoneyMoon/HMUtils.swift
@@ -1,10 +1,10 @@
-extension Array {
-    func every(fn: (T) -> Bool) -> Bool {
-        for item in self {
-            if !fn(item) {
-                return false
-            }
-        }
-        return true
-    }
-}
+//extension Array {
+//    func every(fn: (T) -> Bool) -> Bool {
+//        for item in self {
+//            if !fn(item) {
+//                return false
+//            }
+//        }
+//        return true
+//    }
+//}

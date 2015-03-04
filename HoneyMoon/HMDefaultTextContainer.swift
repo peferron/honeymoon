0b3b0664d9fa33b832e06a28db715cc97fa1b824
peferron <pe.ferron@gmail.com>
2015-03-03 17:49:30 -0800

    Add commented-out implementation of progressive text displaying
    Too slow on simulator, and too irregular on a real device. Need to do this better.

--- HoneyMoon/HMDefaultTextContainer.swift
+++ HoneyMoon/HMDefaultTextContainer.swift
@@ -32,4 +32,50 @@ public class HMDefaultTextContainer: UITextView {
         userInteractionEnabled = false
         resignFirstResponder()
     }
+
+//    func newAttributedString(string: String) -> NSMutableAttributedString {
+//        let att = NSMutableAttributedString(string: string)
+//        att.addAttribute(NSFontAttributeName, value: UIFont.systemFontOfSize(36), range: NSMakeRange(0, att.length))
+//        return att
+//    }
+//
+//    func animateAttributedString() {
+//        let timer = NSTimer.scheduledTimerWithTimeInterval(0.05, target: self, selector: "showFirstCharacter", userInfo: nil, repeats: true)
+//        timer.fire()
+//    }
+//
+//    func showFirstCharacter() {
+//        let att = attributedText.mutableCopy() as! NSMutableAttributedString
+//        if let first = self.indexOfFirstTransparentCharacter(att) {
+//            let characterColor = textColor.colorWithAlphaComponent(1)
+//            att.addAttribute(NSForegroundColorAttributeName, value: characterColor, range: NSMakeRange(first, 1))
+//            self.attributedText = att
+//        }
+//    }
+//
+//    func indexOfFirstTransparentCharacter(att: NSAttributedString) -> Int? {
+//        for i in 0..<att.length {
+//            if let characterColor = att.attribute(NSForegroundColorAttributeName, atIndex: i, effectiveRange: nil) as? UIColor {
+//                if CGColorGetAlpha(characterColor.CGColor) < 1 {
+//                    return i
+//                }
+//            }
+//        }
+//        return nil
+//    }
+//
+//    override public var text: String! {
+//        get {
+//            return super.text
+//        }
+//        set {
+//            let att = newAttributedString(newValue)
+//            for i in 0..<att.length {
+//                let characterColor = textColor.colorWithAlphaComponent(0)
+//                att.addAttribute(NSForegroundColorAttributeName, value: characterColor, range: NSMakeRange(i, 1))
+//            }
+//            attributedText = att
+//            animateAttributedString()
+//        }
+//    }
 }

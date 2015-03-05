773fc2d43cfd565aa70ef5fe3198a37f5c5a963e
peferron <pe.ferron@gmail.com>
2015-03-04 21:13:53 -0800

    Add struct HMState

---
+++ HoneyMoon/HMState.swift
@@ -0,0 +1,9 @@
+public struct HMState {
+    public let scene: String
+    public let step: String
+
+    public init(scene: String, step: String) {
+        self.scene = scene
+        self.step = step
+    }
+}

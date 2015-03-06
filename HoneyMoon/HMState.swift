96b0657905945fb1a1f5e1de1c7ef5bbff50e976
peferron <pe.ferron@gmail.com>
2015-03-05 23:14:01 -0800

    Add state save & load

--- HoneyMoon/HMState.swift
+++ HoneyMoon/HMState.swift
@@ -4,4 +4,23 @@ public struct HMState {
     public init(sceneClass: HMScene.Type) {
         self.sceneClass = sceneClass
     }
+
+    // MARK: - Save & load
+
+    static let sceneClassNameKey = "state.sceneClassName"
+
+    public func save() {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(NSStringFromClass(self.sceneClass), forKey: HMState.sceneClassNameKey)
+        defaults.synchronize()
+    }
+
+    public static func load() -> HMState? {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let sceneClassName = defaults.stringForKey(HMState.sceneClassNameKey)
+        if let sceneClass = NSClassFromString(sceneClassName) as? HMScene.Type {
+            return HMState(sceneClass: sceneClass)
+        }
+        return nil
+    }
 }

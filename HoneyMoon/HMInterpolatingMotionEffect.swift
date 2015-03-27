7a8b0f89e4fd20be6f7036d3099db183b55312d2
peferron <pe.ferron@gmail.com>
2015-03-26 19:57:59 -0700

    Add convenience function for setting up background motion

--- HoneyMoon/HMInterpolatingMotionEffect.swift
+++ HoneyMoon/HMInterpolatingMotionEffect.swift
@@ -1,4 +1,5 @@
 import UIKit
+import SpriteKit
 
 public class HMInterpolatingMotionEffect: UIInterpolatingMotionEffect {
     class var key: String {
@@ -19,5 +20,50 @@ public class HMInterpolatingMotionEffect: UIInterpolatingMotionEffect {
         let value = dict[HMInterpolatingMotionEffect.key] as Float
         callback?(value)
         return [NSObject : AnyObject]()
+    }
+
+    public class func moveBackground(background: SKNode, relativeToView view: UIView) {
+        //        let tiltGroup = childNodeWithName("//tilt_group")!
+        //        var prevPosition = tiltGroup.position
+        //        var prevDate: NSDate = NSDate()
+        //        let groupEffect = HMInterpolatingMotionEffectGroup { (horizontal, vertical) in
+        //            let newPosition = CGPoint(x: CGFloat(-100 * horizontal), y: CGFloat(100 * vertical))
+        //
+        //            let deltaX = newPosition.x - prevPosition.x
+        //            let deltaY = newPosition.y - prevPosition.y
+        //            let deltaPosition = sqrt(deltaX * deltaX + deltaY + deltaY)
+        //
+        //            let newDate = NSDate()
+        //            let deltaDate = newDate.timeIntervalSinceDate(prevDate)
+        //            println("deltaDate: \(deltaDate), deltaPosition: \(deltaPosition)")
+        //
+        //            if abs(deltaPosition) < 1 {
+        //                println("  Skip");
+        //                return
+        //            }
+        //
+        //            tiltGroup.removeAllActions()
+        //            if deltaDate < 1 / 20 {
+        //                println("  Set position");
+        //                tiltGroup.position = newPosition
+        //            } else {
+        //                println("  Animate position")
+        //                let action = SKAction.moveTo(newPosition, duration: deltaDate)
+        //                tiltGroup.runAction(action)
+        //            }
+        //
+        //            prevDate = newDate
+        //            prevPosition = newPosition
+        //        }
+        //        view?.addMotionEffect(groupEffect)
+
+        let deltaX = (background.frame.size.width - view.frame.size.width) / 2
+        let deltaY = (background.frame.size.height - view.frame.size.height) / 2
+
+        let groupEffect = HMInterpolatingMotionEffectGroup { (horizontal, vertical) in
+            let newPosition = CGPoint(x: deltaX * CGFloat(horizontal), y: deltaY * CGFloat(vertical))
+            background.position = newPosition
+        }
+        view.addMotionEffect(groupEffect)
     }
 }
